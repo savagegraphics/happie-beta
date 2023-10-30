521ec18d24fd492dab9962cbb39288bd74bb98c2
savagegraphics <olawaleraji188@gmail.com>
2023-10-30 08:28:44 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,15 @@
-'use client'
+'use client;'
+import React from 'react'
+import Dashboard from './Dashboard/Dashboard'
 
-import { Dropdown, Navbar } from 'flowbite-react'
-import { Avatar } from 'flowbite-react'
+type Props = {}
 
-export default function NavbarWithDropdown () {
+const page = (props: Props) => {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href='https://flowbite-react.com'>
-        <img
-          src='/favicon.svg'
-          className='mr-3 h-6 sm:h-9'
-          alt='Flowbite React Logo'
-        />
-        <span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
-          Flowbite React
-        </span>
-      </Navbar.Brand>
-      <div className='flex md:order-2'>
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar
-              alt='User settings'
-              img='https://flowbite.com/docs/images/people/profile-picture-5.jpg'
-              rounded
-            />
-          }
-        >
-          <Dropdown.Header>
-            <span className='block text-sm'>Bonnie Green</span>
-            <span className='block truncate text-sm font-medium'>
-              name@flowbite.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
-    </Navbar>
+    <div>
+      <Dashboard />
+    </div>
   )
 }
+
+export default page
