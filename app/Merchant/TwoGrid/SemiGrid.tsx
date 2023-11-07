103e1959edfa145a86e1f1eadafab5815ea5feea
savagegraphics <olawaleraji188@gmail.com>
2023-11-07 06:14:55 -0800

    first commit

--- app/Merchant/TwoGrid/SemiGrid.tsx
+++ app/Merchant/TwoGrid/SemiGrid.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 const SemiGrid = (props: Props) => {
   return (
-    <div className='rounded-2xl mt-4 bg-white lg:w-full lg:h-[28rem] w-full h-[30rem]  border border-gray-200'>
+    <div className='rounded-2xl mt-4 bg-white lg:w-full w-full  border border-gray-200'>
       <img src='./assets/Rectangle.png' alt='' className='w-full' />
       <div className='text-[#0F2B48] font-bold text-xl mx-4 my-2'>
         Closest shipping
